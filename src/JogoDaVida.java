3c4fc337b8b5fb1593490fd43cc9b956e1db3701
Marco Souza <marcoasouza.1@gmil.com>
2012-07-01 15:49:06 -0400

    terminado jogo da vida

--- src/JogoDaVida.java
+++ src/JogoDaVida.java
@@ -1,142 +1,150 @@
-import java.util.ArrayList;
-import java.util.List;
-
+import java.io.Console;
 
 public class JogoDaVida {
-
-	private boolean [][] tabuleiro;
 	
-	public JogoDaVida(int lines, int cols) {
-		tabuleiro = new boolean[lines][cols];
+	private static final char VIVA = '*';
+	private static final char MORTA = '.';
+	
+	private boolean [][] tabuleiro;
+	private boolean [][] proximoRodada;
+	private int linhas;
+	private int colunas;
+	
+	public JogoDaVida(int linhas, int colunas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.tabuleiro = new boolean[linhas][colunas];
 	}
 	
-	public boolean getEstado(int x, int y) {
-		return this.tabuleiro[x][y];
+	public JogoDaVida(String tabuleiro){
+		if(tabuleiro.charAt(tabuleiro.length()-1)!='\n'){
+			tabuleiro += "\n";
+		}
+		for(int i=0;i<tabuleiro.length();i++){
+			if(tabuleiro.charAt(i)=='\n'){
+				this.colunas = i;
+				break;
+			}
+		}
+		this.linhas = 0;
+		for(int i=0;i<tabuleiro.length();i++){
+			if(tabuleiro.charAt(i)=='\n'){
+				this.linhas++;
+			}
+		}
+		this.tabuleiro = new boolean[this.linhas][this.colunas];
+		int x = 1, y=1;
+		for(int i=0;i<tabuleiro.length();i++){
+			if(tabuleiro.charAt(i)=='\n'){
+				x++;
+				y=0;
+			}
+			if(tabuleiro.charAt(i)==this.VIVA){
+				avivar(x,y);
+			}
+			y++;
+		}
+	}
+	
+	public String getEstado(int x, int y){
+		return this.getString(this.tabuleiro[x-1][y-1]);
 	}
 	
 	public void avivar(int x, int y){
-		this.tabuleiro[x][y] = true;
+		this.tabuleiro[x-1][y-1] = true;
 	}
 	
 	public void matar (int x, int y){
-		this.tabuleiro[x][y] = false;
+		this.tabuleiro[x-1][y-1] = false;
 	}
 	
-	public void proximaGeracao(){
-		this.avivar(1, 3);
+	public int getQtdVizinhosVivos(int x, int y){
+		int qtdVizinhosVivos = 0;
+		boolean valorCasa = this.tabuleiro[x-1][y-1];
+		for(int i=x-1;i<=x+1;i++){
+			for(int j=y-1;j<=y+1;j++){
+				if(i!=x || j!=y){
+					if(coordenadaExiste(i, j)){
+						if (this.tabuleiro[i-1][j-1]){
+							qtdVizinhosVivos++;
+						}
+					}
+				}
+			}
+		}
+		return qtdVizinhosVivos;
 	}
 	
-	
-	
-	public int vizinhosVivos(int x, int y){
-		
-		int vizinhosVivos = 0;
-		List<Integer> enderecoVizinho;
-		
-		enderecoVizinho = this.getEnderecoVizinho1(x, y);
-		if(enderecoVizinho!=null && this.getEstado(enderecoVizinho)){
-			vizinhosVivos++;
+	private boolean coordenadaExiste(int x, int y){
+		if(x<=0 || x>this.linhas){
+			return false;
 		}
-		enderecoVizinho = this.getEnderecoVizinho2(x, y);
-		if(enderecoVizinho!=null && this.getEstado(enderecoVizinho)){
-			vizinhosVivos++;
+		if(y<=0 || y>this.colunas){
+			return false;
 		}
-		enderecoVizinho = this.getEnderecoVizinho3(x, y);
-		if(enderecoVizinho!=null && this.getEstado(enderecoVizinho)){
-			vizinhosVivos++;
-		}
-		enderecoVizinho = this.getEnderecoVizinho4(x, y);
-		if(enderecoVizinho!=null && this.getEstado(enderecoVizinho)){
-			vizinhosVivos++;
-		}
-		enderecoVizinho = this.getEnderecoVizinho5(x, y);
-		if(enderecoVizinho!=null && this.getEstado(enderecoVizinho)){
-			vizinhosVivos++;
-		}
-		enderecoVizinho = this.getEnderecoVizinho6(x, y);
-		if(enderecoVizinho!=null && this.getEstado(enderecoVizinho)){
-			vizinhosVivos++;
-		}
-		enderecoVizinho = this.getEnderecoVizinho7(x, y);
-		if(enderecoVizinho!=null && this.getEstado(enderecoVizinho)){
-			vizinhosVivos++;
-		}
-		enderecoVizinho = this.getEnderecoVizinho8(x, y);
-		if(enderecoVizinho!=null && this.getEstado(enderecoVizinho)){
-			vizinhosVivos++;
-		}
-		
-		return vizinhosVivos;
-	}
-	
-	private boolean getEstado(List <Integer> coordenada){
-		
-		return this.getEstado(coordenada.get(0), coordenada.get(1));
-	}
-	
-
-	public List<Integer> getEnderecoVizinho1(int x, int y) {
-		return getEnderecoVizinhoN(x, y, -1, -1);
-	}
-	
-	public List<Integer> getEnderecoVizinho2(int x, int y) {
-		return getEnderecoVizinhoN(x, y, -1, 0);
-	}
-	
-	private List<Integer> getEnderecoVizinhoN(int x, int y, int deltaX, int deltaY) {
-	
-		List<Integer> vizinho= null;
-		
-		
-			
-		if(this.existiPosicao(x+deltaX, y+deltaY)) {
-			vizinho= new ArrayList<Integer>();
-			vizinho.add(x+deltaX);
-			vizinho.add(y+deltaY);
-		}
-		
-		return vizinho; 
-		
-	
-	}
-    private boolean existiPosicao(int x, int y){
-    	boolean existePosicao;
-    	
-    	try{
-    		boolean posicao = this.tabuleiro[x][y];
-    		
-			existePosicao = true; 
-			
-			
-		}catch(Exception e){existePosicao= false;}
-    	
-    	return existePosicao;
-    }
-    
-	public List<Integer> getEnderecoVizinho3(int x, int y) {
-		return getEnderecoVizinhoN(x, y, -1, 1);
+		return true;
 	}
 
-	public List<Integer> getEnderecoVizinho4(int x, int y) {
-		return getEnderecoVizinhoN(x, y, 0, -1);
-	}
-
-	public List<Integer> getEnderecoVizinho5(int x, int y) {
-		return getEnderecoVizinhoN(x, y, 0, 1);
-	}
-
-	public List<Integer> getEnderecoVizinho6(int x, int y) {
-		return getEnderecoVizinhoN(x, y, 1, -1);
-	}
-
-	public List<Integer> getEnderecoVizinho7(int x, int y) {
-		return getEnderecoVizinhoN(x, y, 1, 0);
-	}
-
-	public List<Integer> getEnderecoVizinho8(int x, int y) {
-		return getEnderecoVizinhoN(x, y, 1, 1);
+	private String getString(boolean estado){
+		if(estado){
+			return String.valueOf(this.VIVA);
+		} else {
+			return String.valueOf(this.MORTA);
+		}
 	}
 	
+	public String toString(){
+		String retorno = "";
+		for(int i=1;i<=this.linhas;i++){
+			for(int j=1;j<=this.colunas;j++){
+				retorno += getEstado(i,j);
+			}
+			retorno += "\n";
+		}
+		return retorno;
+	}
+
+	public void executar() {
+		this.proximoRodada = new boolean[this.linhas][this.colunas];
+		for(int i=0;i<this.linhas;i++){
+			for(int j=0;j<this.colunas;j++){
+				int qtdVizinhos = getQtdVizinhosVivos(i+1, j+1);
+				if(this.tabuleiro[i][j]){
+					if(qtdVizinhos<2 || qtdVizinhos > 3){
+						this.proximoRodada[i][j] = false;
+					} else {
+						this.proximoRodada[i][j] = true;
+					}
+				} else {
+					if(qtdVizinhos==3){
+						this.proximoRodada[i][j] = true;
+					}
+				}
+			}
+		}
+		this.tabuleiro = this.proximoRodada;
+		this.proximoRodada = null;
+	}
+	
+	public static void main(String args[]){
+		JogoDaVida jogo = new JogoDaVida(10,10);
+		jogo.avivar(8, 8);
+		jogo.avivar(8, 9);
+		jogo.avivar(8, 10);
+		jogo.avivar(9, 8);
+		jogo.avivar(10, 9);
+		for(int i=0;i<50;i++){
+			System.out.print("\n\n");
+			System.out.print(jogo.toString());
+			jogo.executar();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 	
 }
     
--- src/JogoDaVidaTest.java
+++ src/JogoDaVidaTest.java
@@ -6,156 +6,155 @@ import junit.framework.TestCase;
 
 public class JogoDaVidaTest extends TestCase {
 	
+	public void testDeveRetornarOTabuleiroComTodasAsCasasMortas(){
+		String esperado = "....\n....\n....\n....\n";
+		JogoDaVida jogo = new JogoDaVida(4, 4);
+		assertEquals(esperado, jogo.toString());
+	}
+	
+	public void testDeveRetornarEstadoDoTabuleiroComCasasVivas(){
+		JogoDaVida jogo = new JogoDaVida(4, 4);
+		jogo.avivar(1, 2);
+		jogo.avivar(1, 3);
+		jogo.avivar(2, 1);
+		jogo.avivar(2, 4);
+		jogo.avivar(3, 1);
+		jogo.avivar(3, 2);
+		jogo.avivar(3, 3);
+		jogo.avivar(3, 4);
+		String esperado = ".**.\n*..*\n****\n....\n";
+		assertEquals(esperado, jogo.toString());
+	}
+	
 	public void testRecuperaEstadoInicialDe0x0() {
 		JogoDaVida jogo = new JogoDaVida(1, 1);
-		assertFalse(jogo.getEstado(0, 0));
+		assertEquals(".",jogo.getEstado(1, 1));
 	}
 	
 	public void testRecuperaEstadoDe0x0DepoisDeAvivar() {
 		JogoDaVida jogo = new JogoDaVida(1, 1);
-		jogo.avivar(0, 0);
-		assertTrue(jogo.getEstado(0,0));
+		jogo.avivar(1, 1);
+		assertEquals("*", jogo.getEstado(1,1));
 	}
 	
 	public void testRecuperaEstadoDe0x0DepoisDeMatar(){
 		JogoDaVida jogo = new JogoDaVida(1, 1);
-		jogo.avivar(0, 0);
-		jogo.matar(0, 0);
-		assertFalse(jogo.getEstado(0, 0));
+		jogo.avivar(1, 1);
+		jogo.matar(1, 1);
+		assertEquals(".",jogo.getEstado(1, 1));
 	}
 	
 	public void testLancaExcecaoAoRetornarCelulaForaDoTabuleiro(){
 		JogoDaVida jogo = new JogoDaVida(1, 1);
 		try {
-			jogo.getEstado(1, 1);
-			fail("exceção não lançada");
+			jogo.getEstado(2, 2);
+			fail("Excessão esperada não lançada!");
 		} catch (Exception ex) {
 			assertTrue(true);
 		}
 	}
 	
-	public void testGetEnderecoVizinho1paraCentroDe3x3() {
+	public void testRetorna0VizinhosVivos(){
 		JogoDaVida jogo = new JogoDaVida(3, 3);
-		List <Integer> vizinho  = jogo.getEnderecoVizinho1(1, 1);
-		List <Integer> esperado = new ArrayList<Integer>();
-		esperado.add(0);
-		esperado.add(0);
-		assertEquals(esperado, vizinho);
+		assertEquals(0, jogo.getQtdVizinhosVivos(2,2));
 	}
 	
-	public void testGetEnderecoVizinho1paraCentroDe0x0() {
+	public void testRetorna1VizinhoVivo(){
 		JogoDaVida jogo = new JogoDaVida(3, 3);
-		List <Integer> vizinho  = jogo.getEnderecoVizinho1(0, 0);
-		assertEquals(null, vizinho);
+		jogo.avivar(1, 1);
+		assertEquals(1, jogo.getQtdVizinhosVivos(2, 2));
 	}
 	
-	public void testGetEnderecoVizinho2paraCentroDe3x3() {
+	public void testRetorna8VizinhosVivos(){
 		JogoDaVida jogo = new JogoDaVida(3, 3);
-		List <Integer> vizinho  = jogo.getEnderecoVizinho2(1, 1);
-		List <Integer> esperado = new ArrayList<Integer>();
-		esperado.add(0);
-		esperado.add(1);
-		assertEquals(esperado, vizinho);
+		jogo.avivar(1, 1);
+		jogo.avivar(1, 2);
+		jogo.avivar(1, 3);
+		jogo.avivar(2, 1);
+		jogo.avivar(2, 3);
+		jogo.avivar(3, 1);
+		jogo.avivar(3, 2);
+		jogo.avivar(3, 3);
+		assertEquals(8, jogo.getQtdVizinhosVivos(2, 2));
 	}
 	
-	public void testGetEnderecoVizinho2paraCentroDe0x0() {
+	public void testRetorna0VizinhosVivosDeUmaCasaViva(){
 		JogoDaVida jogo = new JogoDaVida(3, 3);
-		List <Integer> vizinho  = jogo.getEnderecoVizinho2(0, 0);
-		assertEquals(null, vizinho);
+		jogo.avivar(2, 2);
+		assertEquals(0, jogo.getQtdVizinhosVivos(2, 2));
 	}
 	
-	public void testGetEnderecoVizinho3paraCentroDe3x3() {
-		JogoDaVida jogo = new JogoDaVida(3, 3);
-		List <Integer> vizinho  = jogo.getEnderecoVizinho3(1, 1);
-		List <Integer> esperado = new ArrayList<Integer>();
-		esperado.add(0);
-		esperado.add(2);
-		assertEquals(esperado, vizinho);
-	}	
-	public void testGetEnderecoVizinho4paraCentroDe3x3() {
-		JogoDaVida jogo = new JogoDaVida(3, 3);
-		List <Integer> vizinho  = jogo.getEnderecoVizinho4(1, 1);
-		List <Integer> esperado = new ArrayList<Integer>();
-		esperado.add(1);
-		esperado.add(0);
-		assertEquals(esperado, vizinho);
-	}	
-	public void testGetEnderecoVizinho5paraCentroDe3x3() {
-		JogoDaVida jogo = new JogoDaVida(3, 3);
-		List <Integer> vizinho  = jogo.getEnderecoVizinho5(1, 1);
-		List <Integer> esperado = new ArrayList<Integer>();
-		esperado.add(1);
-		esperado.add(2);
-		assertEquals(esperado, vizinho);
-	}	
-	public void testGetEnderecoVizinho6paraCentroDe3x3() {
-		JogoDaVida jogo = new JogoDaVida(3, 3);
-		List <Integer> vizinho  = jogo.getEnderecoVizinho6(1, 1);
-		List <Integer> esperado = new ArrayList<Integer>();
-		esperado.add(2);
-		esperado.add(0);
-		assertEquals(esperado, vizinho);
-	}		
-	public void testGetEnderecoVizinho7paraCentroDe3x3() {
-		JogoDaVida jogo = new JogoDaVida(3, 3);
-		List <Integer> vizinho  = jogo.getEnderecoVizinho7(1, 1);
-		List <Integer> esperado = new ArrayList<Integer>();
-		esperado.add(2);
-		esperado.add(1);
-		assertEquals(esperado, vizinho);
-	}		
-	public void testGetEnderecoVizinho8paraCentroDe3x3() {
-		JogoDaVida jogo = new JogoDaVida(3, 3);
-		List <Integer> vizinho  = jogo.getEnderecoVizinho8(1, 1);
-		List <Integer> esperado = new ArrayList<Integer>();
-		esperado.add(2);
-		esperado.add(2);
-		assertEquals(esperado, vizinho);
-	}		
-	
-	public void testGetVizinhosVivosDeveSer0ComTabuleiroVazio(){
-		JogoDaVida jogo = new JogoDaVida(3, 3);
-		assertEquals(0, jogo.vizinhosVivos(0,0));
+	public void testRetorna0VizinhosVivosDeUmaCasaNoCanto(){
+		JogoDaVida jogo = new JogoDaVida(2, 2);
+		assertEquals(0, jogo.getQtdVizinhosVivos(1, 1));
 	}
 	
-	public void testVizinhosVivosDeveConta1VizinhoVivo(){
-		JogoDaVida jogo = new JogoDaVida(3, 3);
-		jogo.avivar(0,1);
-		assertEquals(1, jogo.vizinhosVivos(0,0));
+	public void testRetorna1VizinhosVivosDeUmaCasaNoCanto(){
+		JogoDaVida jogo = new JogoDaVida(2, 2);
+		jogo.avivar(2, 2);
+		assertEquals(1, jogo.getQtdVizinhosVivos(1, 1));
 	}
 	
-	public void testVizinhosVivosDeveConta8VizinhoVivo(){
-		JogoDaVida jogo = new JogoDaVida(3, 3);
-		jogo.avivar(0,0);
-		jogo.avivar(0,1);
-		jogo.avivar(0,2);
-		jogo.avivar(1,0);
-		jogo.avivar(1,1);
-		jogo.avivar(1,2);
-		jogo.avivar(2,0);
-		jogo.avivar(2,1);
-		jogo.avivar(2,2);
-		assertEquals(8, jogo.vizinhosVivos(1,1));
+	public void testDeveLancarExcessaoCasaForaDoTbuleiro(){
+		JogoDaVida jogo = new JogoDaVida(2, 2);
+		try{
+			assertEquals(1, jogo.getQtdVizinhosVivos(3, 1));
+			fail("Excessão esperada não lançada!");
+		} catch(Exception e ){
+			assertTrue(true);
+		}
 	}
 	
-	
-	
-	public void testCelula1x3DeveNascerNaGeracao1(){
-		JogoDaVida jogo = new JogoDaVida(4, 8);
-		jogo.avivar(1,4);
-		jogo.avivar(2,3);
-		jogo.avivar(2,4);
-		jogo.proximaGeracao();
-		assertTrue(jogo.getEstado(1,3));
+	public void testDeveCriarOTabuleiroAPartirDeUmaString(){
+		String tabuleiro = "..\n..\n";
+		JogoDaVida jogo = new JogoDaVida(tabuleiro);
+		assertEquals(tabuleiro, jogo.toString());
 	}
 	
-	
-	public void testCelula2x4DeveMorrerNaGeracao1(){
-		JogoDaVida jogo = new JogoDaVida(4, 8);
-		jogo.avivar(2,4);
-		jogo.proximaGeracao();
-		assertFalse(jogo.getEstado(2, 4));
-		
+	public void testDeveCriarOTabuleiroNãoQuadradoAPartirDeUmaString(){
+		String tabuleiro = "....\n....\n";
+		JogoDaVida jogo = new JogoDaVida(tabuleiro);
+		assertEquals(tabuleiro, jogo.toString());
 	}
 	
+	public void testDeveCriarOTabuleiroEAvivarUmaCasaAPartirDeUmaString(){
+		String tabuleiro = ".*\n.*\n";
+		JogoDaVida jogo = new JogoDaVida(tabuleiro);
+		assertEquals(tabuleiro, jogo.toString());
+	}
+	
+	public void testCelulaComMenosDeDoisVizinhosVivosDeveMorrer(){
+		JogoDaVida jogo = new JogoDaVida("*.\n..\n");
+		jogo.executar();
+		assertEquals(".", jogo.getEstado(1, 1));
+	}
+	
+	public void testCelulaVivaComMaisDeTresVizinhosDeveMorrer(){
+		JogoDaVida jogo = new JogoDaVida(".*.\n**.\n**.");
+		jogo.executar();
+		assertEquals(".", jogo.getEstado(2, 1));
+		assertEquals(".", jogo.getEstado(2, 2));
+	}
+	
+	public void testCelulaMortaCom3VizinhosVivosDeveViver(){
+		JogoDaVida jogo = new JogoDaVida(".*.\n**.\n...");
+		jogo.executar();
+		assertEquals("*",jogo.getEstado(1, 1));
+	}
+	
+	public void testCelulaVivaCom3Ou2VizinhosVivosDeveContinuarViva(){
+		JogoDaVida jogo = new JogoDaVida(".*.\n*.*\n.*.");
+		jogo.executar();
+		assertEquals(".*.\n*.*\n.*.\n",jogo.toString());
+	}
+	
+	public void testPadraoFigura(){
+		JogoDaVida jogo = new JogoDaVida("...\n***\n...");
+		jogo.executar();
+		assertEquals(".*.\n.*.\n.*.\n",jogo.toString());
+		jogo.executar();
+		assertEquals("...\n***\n...\n",jogo.toString());
+		jogo.executar();
+		assertEquals(".*.\n.*.\n.*.\n",jogo.toString());
+	}
 }
